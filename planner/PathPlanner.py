c0c118f386c882bd0ce1e021d53131acd1beb7d9
rohanb2018 <rohanb@Rohans-MacBook-Pro.local>
2014-10-02 00:21:51 -0400

    Comments
    Adding comments to PathPlanner

--- planner/PathPlanner.py
+++ planner/PathPlanner.py
@@ -4,7 +4,10 @@ class PathPlanner(object):
         super(PathPlanner, self).__init__()
 
     def step(self, world):
-    	quad = []
+    	
+        # 3 lists that store quad data, roombas to target and roombas to avoid
+        
+        quad = []
     	roomba_target = []
     	roomba_avoid = []
     	output = {}
@@ -12,6 +15,8 @@ class PathPlanner(object):
     	width = 200
     	tick = 1
 
+        # Putting things into different lists
+        
     	for key in world:
     		if key == 'quad':
     			quad.append(world[key])
@@ -19,31 +24,33 @@ class PathPlanner(object):
     			roomba_avoid.append(world[key])
     		else:
     			roomba_target.append(world[key])
+                
+        
 
-    	difference = roomba_target['position'].sub(quad['position'])
-    	t = path.magnitude()/quad['maxvelocity'] 
+    	difference = roomba_target['position'].sub(quad['position'])    # difference between your position and roombas position
+    	t = difference.magnitude()/quad['maxvelocity'] 
     	bayesian_t = t * quad['maxvelocity']/roomba_target['velocity'].magnitude()
 
-    	path_o = roomba_target['position'] + roomba_target['velocity'].scale(bayesian_t / 2)
-    	path = roomba_target['position'] + roomba_target['velocity'].scale(bayesian_t)
+    	path_o = roomba_target['position'].add(roomba_target['velocity'].scale(bayesian_t / 2))
+    	path = roomba_target['position'].add(roomba_target['velocity'].scale(bayesian_t))
 
-    	output['move_%d' % tick] = (path[0], path[1], quad['position'][2])
+    	output['move_%d' % tick] = (path[0], path[1], quad['position'][2]) #(path[0] = x, path[1], quad['position'][2])
     	tick += 1
-    	output['descend_%d' % tick] = path
+    	output['descend_%d' % tick] = path # path has z-ccordinate = 0
     	tick += 1
 
-    	if path[1] < length/2:
-    		if roomba_target['velocity'].quadrant() == 1:
+    	if path[0] < length/2: # roomba on left side of grid
+    		if roomba_target['velocity'].quadrant() == 2:
     			output['move_%d' % tick] = path.add(roomba_target['velocity'].scale(-1))
     			tick += 1
     			path.add(roomba_target['velocity'].scale(-1))
-    			output['ascend_%d' % tick] = (path[0], path[1], 0.7)
+    			output['ascend_%d' % tick] = (path[0], path[1], 0.7) # ascend to height
     			tick += 1
     			output['descend_%d' % tick] = path.add(roomba_target['velocity'].scale(0.3))
     			tick += 1
     		else if roomba_target['velocity'].quadrant() == 3:
     			output['descend_%d' % (tick - 1)] = path_o
-    	else:
+    	else:   # roomba on right side of grid
     		if roomba_target['velocity'].quadrant() == 1:
     			output['descend_%d' % (tick - 1)] = path_o
     		else if roomba_target['velocity'].quadrant() == 3:
